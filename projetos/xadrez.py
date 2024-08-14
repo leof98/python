33ba40bb138aa79d1ef88e26a0f2c8cf8efdb8e1
Leonardo Franco <lf290198@gmail.com>
2024-08-14 15:59:14 -0300

    Create xadrez.py

---
+++ projetos/xadrez.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+# Constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
+BOARD_SIZE = 8
+SQUARE_SIZE = SCREEN_WIDTH // BOARD_SIZE
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Pieces images
+PIECE_IMAGES = {}
+
+# Initialize pygame
+pygame.init()
+
+# Set up display
+screen = pygame.display.###
