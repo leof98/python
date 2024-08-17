05bc141e135b142173beee6396550973a9bd2003
Leonardo Franco <lf290198@gmail.com>
2024-08-17 10:48:08 -0300

    Update xadrez.py

--- projetos/xadrez.py
+++ projetos/xadrez.py
@@ -5,8 +5,8 @@ import sys
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
 BOARD_SIZE = 8
 SQUARE_SIZE = SCREEN_WIDTH // BOARD_SIZE
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+WHITE = (240, 217, 181)
+BLACK = (181, 136, 99)
 
 # Pieces images
 PIECE_IMAGES = {}
@@ -21,7 +21,8 @@ pygame.display.set_caption('Chess')
 
 # Load images
 def load_images():
-    pieces = ['wp', 'bp', 'wr', 'br', 'wn', 'bn', 'wb', 'bb', 'wq', 'bq', 'wk', 'bk']
+    pieces = ['w_pawn', 'w_rook', 'w_knight', 'w_bishop', 'w_queen', 'w_king',
+              'b_pawn', 'b_rook', 'b_knight', 'b_bishop', 'b_queen', 'b_king']
     for piece in pieces:
         PIECE_IMAGES[piece] = pygame.transform.scale(pygame.image.load(f'images/{piece}.png'), (SQUARE_SIZE, SQUARE_SIZE))
 
@@ -41,7 +42,7 @@ class Piece:
 class Pawn(Piece):
     def __init__(self, color):
         super().__init__(color)
-        self.image = PIECE_IMAGES[f'{color}p']
+        self.image = PIECE_IMAGES[f'{color}_pawn']
 
     def get_moves(self, board, row, col):
         #TODO
@@ -50,7 +51,7 @@ class Pawn(Piece):
 class Rook(Piece):
     def __init__(self, color):
         super().__init__(color)
-        self.image = PIECE_IMAGES[f'{color}r']
+        self.image = PIECE_IMAGES[f'{color}_rook']
 
     def get_moves(self, board, row, col):
         #TODO
@@ -59,7 +60,7 @@ class Rook(Piece):
 class Knight(Piece):
     def __init__(self, color):
         super().__init__(color)
-        self.image = PIECE_IMAGES[f'{color}n']
+        self.image = PIECE_IMAGES[f'{color}_knight']
 
     def get_moves(self, board, row, col):
         #TODO
@@ -68,7 +69,7 @@ class Knight(Piece):
 class Bishop(Piece):
     def __init__(self, color):
         super().__init__(color)
-        self.image = PIECE_IMAGES[f'{color}b']
+        self.image = PIECE_IMAGES[f'{color}_bishop']
 
     def get_moves(self, board, row, col):
         #TODO
@@ -77,7 +78,7 @@ class Bishop(Piece):
 class Queen(Piece):
     def __init__(self, color):
         super().__init__(color)
-        self.image = PIECE_IMAGES[f'{color}q']
+        self.image = PIECE_IMAGES[f'{color}_queen']
 
     def get_moves(self, board, row, col):
         #TODO
@@ -86,7 +87,7 @@ class Queen(Piece):
 class King(Piece):
     def __init__(self, color):
         super().__init__(color)
-        self.image = PIECE_IMAGES[f'{color}k']
+        self.image = PIECE_IMAGES[f'{color}_king']
 
     def get_moves(self, board, row, col):
         #TODO
@@ -102,39 +103,39 @@ class Board:
 
         # Placing pawns on the board
         for i in range(BOARD_SIZE):
-            board[1][i] = Pawn('w') # White pawns
-            board[6][i] = Pawn('b') # Black pawns
+            board[6][i] = Pawn('w') # White pawns
+            board[1][i] = Pawn('b') # Black pawns
 
         # Placing rooks on the board
-        board[0][0] = board[0][7] = Rook('w')
-        board[7][0] = board[7][7] = Rook('b')
+        board[7][0] = board[7][7] = Rook('w')
+        board[0][0] = board[0][7] = Rook('b')
 
         # Placing knights on the board
-        board[0][1] = board[0][6] = Knight('w')
-        board[7][1] = board[7][6] = Knight('b')
+        board[7][1] = board[7][6] = Knight('w')
+        board[0][1] = board[0][6] = Knight('b')
 
         # Placing bishops on the board
-        board[0][2] = board[0][5] = Bishop('w')
-        board[7][2] = board[7][5] = Bishop('b')
+        board[7][2] = board[7][5] = Bishop('w')
+        board[0][2] = board[0][5] = Bishop('b')
 
         # Placing queens on the board
-        board[0][3] = Queen('w')
-        board[7][3] = Queen('b')
+        board[7][3] = Queen('w')
+        board[0][3] = Queen('b')
 
         # Placing kings on the board
-        board[0][4] = King('w')
-        board[7][4] = King('b')
+        board[7][4] = King('w')
+        board[0][4] = King('b')
 
         return board
     
-def draw(self, screen):
-        for row in range(BOARD_SIZE):
-            for col in range(BOARD_SIZE):
-                color = WHITE if (row + col) % 2 == 0 else BLACK
-                pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                piece = self.board[row][col]
-                if piece:
-                    piece.draw(screen, row, col)
+    def draw(self, screen):
+            for row in range(BOARD_SIZE):
+                for col in range(BOARD_SIZE):
+                    color = WHITE if (row + col) % 2 == 0 else BLACK
+                    pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                    piece = self.board[row][col]
+                    if piece:
+                        piece.draw(screen, row, col)
 
 class Game:
     def __init__(self):
@@ -154,4 +155,4 @@ class Game:
 if __name__ == '__main__':
     load_images()
     game = Game()
-    game.run()   
+    game.run()
