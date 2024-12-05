ac38829cd1ef90004f8755c4258bde804b31c646
Leonardo Franco <lf290198@gmail.com>
2024-12-05 17:44:54 -0300

    Create day5.py

---
+++ projetos/100days/day5.py
@@ -0,0 +1,71 @@
+# Day 5
+
+# Ex 1 - FizzBuzz Game
+for number in range(1, 101):
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
+
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# password = ""
+# # Letters
+# for item in range(0, nr_letters):
+#     password += random.choice(letters)
+#
+# # Symbols
+# for item in range(0, nr_symbols):
+#     password += random.choice(symbols)
+#
+# # Numbers
+# for item in range(0, nr_symbols):
+#     password += random.choice(numbers)
+#
+# print(password)
+# pass_list = []
+# for i in password:
+#     pass_list.append(i)
+# random.shuffle(pass_list)
+#
+# # Printing the password
+# for i in password:
+#     print(i, end="")
+
+password = []
+# Letters
+for item in range(0 ,nr_letters):
+    password.append(random.choice(letters))
+
+# Symbols
+for item in range(0, nr_symbols):
+    password.append(random.choice(symbols))
+
+# Numbers
+for item in range(0, nr_numbers):
+    password.append(random.choice(numbers))
+
+random.shuffle(password)
+for char in password:
+    print(char, end="")
+
+
+'''
+Notes - Day 5
+-> Loops
+-> Range
+'''
