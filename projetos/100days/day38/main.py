fa4cd7c5131d6d77565b8484dc923c668ac806c3
Leonardo Franco <lf290198@gmail.com>
2025-01-02 14:08:19 -0300

    Create main.py

---
+++ projetos/100days/day38/main.py
@@ -0,0 +1,35 @@
+# Day 38
+# HTTP Requests
+# I need to review this day.
+
+import requests
+
+
+GENDER = "male"
+WEIGHT_KG = 60
+HEIGHT_CM = 170
+AGE = 26
+
+APP_ID = "0fbda080"
+API_KEY = "4a4434592ecd13dfbe352dcc98184501"
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me what you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE,
+}
+
+response = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
