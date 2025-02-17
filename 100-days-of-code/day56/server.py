ddbd011d3ac36f3b91b3746f7264dbfb34423fc8
Leonardo Franco <lf290198@gmail.com>
2025-02-17 11:04:35 -0300

    Create server.py

---
+++ 100-days-of-code/day56/server.py
@@ -0,0 +1,13 @@
+# Day 56
+# more on Flask, using templates
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
